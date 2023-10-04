860df5d8c2fb709628fda91ccfd22bbbbef8afb4
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-10-05 00:26:31 +0530

    fetch data from google search API 1

--- google-clone-nextjs13/src/app/search/web/page.jsx
+++ google-clone-nextjs13/src/app/search/web/page.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function webSearchPage() {
+async function webSearchPage() {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.SEARCH_CONTEXT_KEY}&q=lecture`
+  )
+  const data = await response.json();
+  console.log(data);
+  const results = data.items
   return (
-    <div>webSearchPage</div>
+    <>
+      {results && results.map((result) => <h1>{result.title}</h1>)}
+    </>
   )
 }
-
+ 
 export default webSearchPage
