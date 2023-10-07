d0d20be32158a7c89aada6063d79ede846f4ad8e
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-10-07 10:38:52 +0530

    fwtch data from google search API complete and Error handle

--- google-clone-nextjs13/src/app/search/web/page.jsx
+++ google-clone-nextjs13/src/app/search/web/page.jsx
@@ -1,12 +1,28 @@
+import Link from 'next/link';
 import React from 'react'
 
-async function webSearchPage() {
+async function webSearchPage({searchParams}) {
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.SEARCH_CONTEXT_KEY}&q=lecture`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.SEARCH_CONTEXT_KEY}&q=${searchParams.searchTerm}`
   )
   const data = await response.json();
-  console.log(data);
+
+  if(!response.ok) {
+    throw new Error("Something went wrong")
+  }
   const results = data.items
+  if(!results) {
+    return <div className='flex flex-col justify-center items-center pt-10'>
+      <h1 className='text-3xl mb-4'>No results found</h1>
+      <p className='text-lg'>Try searching for something else or go back to the homepage {" "}
+
+        <Link href="/" className='text-blue-500'>
+          Home
+        </Link>
+      </p>
+      
+      </div>
+  }
   return (
     <>
       {results && results.map((result) => <h1>{result.title}</h1>)}
