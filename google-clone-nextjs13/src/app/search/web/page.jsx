a388add83f95e45b1b48355028890fc8ece3c7b9
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-10-07 17:24:54 +0530

    create search result component

--- google-clone-nextjs13/src/app/search/web/page.jsx
+++ google-clone-nextjs13/src/app/search/web/page.jsx
@@ -1,7 +1,9 @@
+import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link';
 import React from 'react'
 
 async function webSearchPage({searchParams}) {
+  await new Promise((resolve) => setTimeout(resolve , 10000))
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.SEARCH_CONTEXT_KEY}&q=${searchParams.searchTerm}`
   )
@@ -25,7 +27,7 @@ async function webSearchPage({searchParams}) {
   }
   return (
     <>
-      {results && results.map((result) => <h1>{result.title}</h1>)}
+      {results && <WebSearchResults results = {data}/>}
     </>
   )
 }
---
+++ google-clone-nextjs13/src/components/WebSearchResults.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+import parser from 'html-react-parser'
+
+function WebSearchResults({ results }) {
+  return (
+    <div className ="w-full mx-auto px-3 sm:pb-24 md:pb-36 lg:pb-32 sm:pr-52 md:pr-52 lg:pr-52 sm:pl-[5%] md:pl-[14%] lg:pl-52 min-h-[100vh] flex flex-col justify-between">
+      <p className="text-gray-600 text-sm mb-5 mt-3">
+        About {results.searchInformation?.formattedTotalResults} results (
+        {results.searchInformation?.formattedSearchTime} seconds)
+      </p>
+      {results.items?.map((result) => (
+        <div className="mb-8 max-width-xl" key={result.link}>
+            <div className="group flex flex-col">
+                <Link className="text-sm truncate" href={result.link}>
+                    {result.formattedUrl}
+                </Link>
+                <Link className="group-hover:underline decoration-blue-800 text-xl truncate font-medium text-blue-800" href={result.link}>
+                    {result.title}
+                </Link>
+            </div>
+             <p className="text-gray-600">{parser(result.htmlSnippet)}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default WebSearchResults;
