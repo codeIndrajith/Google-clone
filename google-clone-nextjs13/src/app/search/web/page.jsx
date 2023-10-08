838427adea5f02e2661095b13eb4c5f89e613304
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-10-08 13:32:53 +0530

    add loading effects for web and image pages

--- google-clone-nextjs13/src/app/search/web/page.jsx
+++ google-clone-nextjs13/src/app/search/web/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 async function webSearchPage({searchParams}) {
   const startIndex = searchParams.start || "1";
-  //  await new Promise((resolve) => setTimeout(resolve , 10000))
+   await new Promise((resolve) => setTimeout(resolve , 5000))
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.SEARCH_CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
   )
