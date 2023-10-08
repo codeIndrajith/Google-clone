2678631c51db340832ea87f3640d2d4f73b2f85e
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-10-08 14:15:59 +0530

    Update countryLookup but not work ip geolocation

--- google-clone-nextjs13/src/components/CountryLookup.jsx
+++ google-clone-nextjs13/src/components/CountryLookup.jsx
@@ -6,9 +6,35 @@ function CountryLookup() {
   const [country, setCountry] = useState('Sri Lanka');
 
   // useEffect(() => {
-  //     fetch(`http://api.ipstack.com/134.201.250.155?key=${process.env.NEXT_IP_API_KEY}`)
-  //       .then((res) => res.json())
-  //       .then((data) => console.log(data));
+  //     // fetch user;s current location
+  //     const fetchUserLocation = () => {
+  //       if(navigator.geolocation) {
+  //         navigator.geolocation.getCurrentPosition(
+  //           async (position) => {
+  //             const {latitude , longitude} = position.coords;
+
+  //             try {
+  //               const response = await fetch(
+  //                 `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${NEXT_IP_API_KEY}`
+
+  //               );
+  //               const data = await response.json();
+
+  //               if (data.results.length > 0) {
+  //                 // Assuming that the country is in the first result
+  //                 const userCountry = data.results[0].components.country;
+  //                 setCountry(userCountry);                
+  //               } else {
+  //                 console.error("Geolocation is not supported in this browser.");
+  //               }
+  //             } catch (error) {
+  //               console.log(error);
+  //             }
+  //           }
+  //         )
+  //       }
+  //     }
+  //     fetchUserLocation();
   // }, []);
   return <div>{country}</div>;
 }
